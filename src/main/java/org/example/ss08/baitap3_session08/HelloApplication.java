c3746d2dba5843ca072df0f3e415790ac5d3e3d3
Bichngocc07 <ngoc01072006@gmail.com>
2026-04-15 19:54:44 +0700

    add baitap_session08

---
+++ src/main/java/org/example/ss08/baitap3_session08/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.ss08.baitap3_session08;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
